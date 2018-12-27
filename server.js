ea8b2178cc4f0b2c1ef82bcbe0e3841f3ecfa862
hronkko <harri.ronkko@gmail.com>
2018-12-27 12:00:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-const apiKey = 'xxxxxxxxxxxxxx;
+const apiKey = 'xxxxxxxxxxxxxx';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
